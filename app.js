b79af5527bf377914362e2e11c8cac27af24de8c
aarons95 <aslater95@icloud.com>
2021-11-03 21:30:13 +0000

    amended sessionConfig variable

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ const sessionConfig = {
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
-    //secure: true,
+    secure: true, //comment out if running local
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
