8e7d35ce9e476b30a6698d8add61217f3f81f555
AaronS95 <aaron5770@hotmail.com>
2024-09-02 23:48:07 +0100

    Updated seeds/index.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 const mongoose = require("mongoose");
 const cities = require("./cities");
 const { places, descriptors } = require("./seedHelpers");
 const Campground = require("../models/campground");
 
-mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+mongoose.connect(process.env.DB_URL || "mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
